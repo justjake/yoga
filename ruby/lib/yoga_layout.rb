6859919b64f007a49c92dca43777faed8c972632
Jake Teton-Landis <jake.tl@airbnb.com>
2017-09-16 19:29:08 -0700

    [ruby] spike

--- ruby/lib/yoga_layout.rb
+++ ruby/lib/yoga_layout.rb
@@ -1,6 +1,111 @@
 require "yoga_layout/version"
 require "yoga_layout/yoga_layout"
+require "ffi"
 
 module YogaLayout
-  # Your code goes here...
+  def self.underscore(string)
+    string.gsub(/([A-Z]+)([A-Z][a-z])/,'\1_\2').
+      gsub(/([a-z\d])([A-Z])/,'\1_\2').
+      downcase
+  end
+
+  # Native is the libffi wrapper around Yoga.
+  # It does not implement anything novel like memory management or nice ruby types.
+  # It's just a compatibility layer.
+  module Native
+    extend FFI::Library
+    # ref https://github.com/ffi/ffi/wiki/Loading-Libraries#in-memory-libraries
+    # I think Yoga should have been loaded by the native xtension yoga_layout/yoga_layout
+    ffi_lib ::FFI::USE_THIS_PROCESS_AS_LIBRARY
+
+    # This file is auto-genrated by ../../enums.py
+    require 'yoga_layout/enums'
+
+    def self.functions
+      @functions ||= {}
+    end
+
+    # Wrapper around FFI::Library.attach_function that also remembers the function
+    # name and arguments for later static analysis
+    def self.remember_function(*args)
+      result = attach_function(*args)
+      functions[args.first] = args
+      result
+    end
+
+
+    typedef :pointer, :YGNodeRef
+
+    # This is adapted by hand from the C API docs,
+    # ref https://facebook.github.io/yoga/docs/api/c/
+
+    # Lifecycle
+    remember_function :YGNodeNew, [], :YGNodeRef
+    remember_function :YGNodeReset, [:YGNodeRef], :void
+    remember_function :YGNodeFree, [:YGNodeRef], :void
+    remember_function :YGNodeFreeRecursive, [:YGNodeRef], :void
+
+    # Children
+    remember_function :YGNodeInsertChild, [:YGNodeRef, :YGNodeRef, :uint32_t], :void
+    remember_function :YGNodeRemoveChild, [:YGNodeRef, :YGNodeRef], :void
+    remember_function :YGNodeGetChild, [:YGNodeRef, :uint32_t], :YGNodeRef
+    remember_function :YGNodeGetChildCount, [:YGNodeRef], :uint32_t
+
+    # Printing (for debug???)
+    remember_function :YGNodePrint, [:YGNodeRef, PrintOptions], :void
+  end
+
+  class Node
+    def self.new_unsafe
+      node = YogaLayout::Native.YGNodeNew
+      new(node)
+    end
+
+    def initialize(pointer)
+      @pointer = pointer
+    end
+
+    def self.map_method(ruby_name, native_name)
+      # look up function definition info that was remembered
+      info = ::YogaLayout::Native.functions[native_name]
+      if info.nil?
+        raise ArgumentError, "Function #{native_name.inspect} not defined in YogaLayout::Native"
+      end
+
+      reciever, *args = info[1]
+      unless reciever == :YGNodeRef
+        raise ArgumentError, "Function #{native_name.inspect} does not take a YGNodeRef as arg[0]"
+      end
+
+      args_list = args.each_with_index.map do |a, i|
+        as_string = if a.is_a?(Symbol)
+                      a.to_s.gsub(/^YG/, '')
+                    elsif a.respond_to?(:name)
+                      a.name.to_s.split('::').last
+                    else
+                      "unknown_#{i}"
+                    end
+
+        YogaLayout.underscore(as_string)
+      end.join(', ')
+
+      class_eval <<-EOS
+        def #{ruby_name}(#{args_list})
+          ::YogaLayout::Native.#{native_name}(@pointer, #{args_list})
+        end
+      EOS
+    end
+
+    # Automagically map every function that recieves a YGNodeRef as the first
+    # value.
+    ::YogaLayout::Native.functions.values.each do |fn_info|
+      native_name, args = fn_info
+      native_name = native_name.to_s
+      next unless native_name =~ /^YGNode/
+      next unless args.first == :YGNodeRef
+
+      ruby_name = YogaLayout.underscore(native_name.gsub(/^YGNode/, ''))
+      map_method(ruby_name.to_sym, native_name.to_sym)
+    end
+  end
 end
--- ruby/spec/yoga_layout_spec.rb
+++ ruby/spec/yoga_layout_spec.rb
@@ -4,8 +4,49 @@ RSpec.describe YogaLayout do
   it "has a version number" do
     expect(YogaLayout::VERSION).not_to be nil
   end
+end
 
-  it "does something useful" do
-    expect(false).to eq(true)
+RSpec.describe YogaLayout::Native do
+  it 'can lifecycle' do
+    node = described_class.YGNodeNew
+    described_class.YGNodeFree(node)
+  end
+
+  it 'can print' do
+    node = described_class.YGNodeNew
+    described_class.YGNodePrint(node, 4)
+    described_class.YGNodeFree(node)
+  end
+end
+
+RSpec.describe YogaLayout::Node do
+  let(:native_nodes) { [] }
+
+  def new_native_node
+    node = YogaLayout::Native.YGNodeNew
+    native_nodes << node
+    node
+  end
+
+  after :each do
+    native_nodes.each { |p| YogaLayout::Native.YGNodeFree(p) }
+  end
+
+  subject { described_class.new(new_native_node) }
+
+  describe '#get_child_count' do
+    it 'returns 0' do
+      expect(subject.get_child_count).to eq(0)
+    end
+
+    context 'with a child' do
+      before do
+        subject.insert_child(new_native_node, 0)
+      end
+
+      it 'returns 1' do
+        expect(subject.get_child_count).to eq(1)
+      end
+    end
   end
 end
